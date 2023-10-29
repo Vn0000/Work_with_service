dedf720ebfaba2d55dfd0f2f751b707a69a08f00
Vn0000 <workmailkonovalov@gmail.com>
2023-10-29 14:12:36 +0400

    Base version with gradle.

---
+++ app/src/main/java/com/bignerdranch/android/workwithservice/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.workwithservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.workwithservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
