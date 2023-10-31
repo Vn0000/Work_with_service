0d853183fa0235bf179328397f1fc46e8e6670de
Vn0000 <workmailkonovalov@gmail.com>
2023-10-31 20:17:37 +0400

    промежуточный комит

--- app/src/main/java/com/bignerdranch/android/workwithservice/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/workwithservice/MainActivity.kt
@@ -10,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
     }
 }
